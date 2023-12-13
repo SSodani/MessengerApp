0b1a764749076838d9521e1dfa756fd8255d3ae3
sonamsodani <76121164+sonamsodani@users.noreply.github.com>
2023-12-13 01:34:20 -0500

    added github pre commit hook

---
+++ MessengerApp/MessengerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MessengerApp
+//
+//  Created by Sonam Sodani on 2023-12-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MessengerApp/MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by Sonam Sodani on 2023-12-13.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
